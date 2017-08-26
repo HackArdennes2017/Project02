8af5b13a27ecf65f64ea6b29a24c348519bf4d2c
Jolan <jolan.maggiochi@epitech.eu>
2017-08-26 09:47:53 +0200

    ok

--- WebApplication/View/Outay.php
+++ WebApplication/View/Outay.php
@@ -2,10 +2,30 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/Outay.css">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<title>Test</title>
-	coucou
 </head>
 <body>
+    <div class="content">
+        <div class="menu">
+          <ul>
+            <li prop='1'>Group 1</li> 
+            <li prop='2'>Group 2</li> 
+            <li prop='3'>Group 3</li> 
+          </ul>
+		    <div class="container">
+		            <div class="product">
+		                 
+		                  <p prop='orange' class="orange block" title="select orange" href="#orange">groupe 1</p>
+		              
+		                  <p prop='cellcom' class="cellcom block" title="select cellcom" href="#cellcom">groupe 2</p>
+		                
+		                  <p prop='pelephone' class="pelephone block" title="select pelephone" href="#pelephone">groupe 3</p>		               
+		            </div>
+		     </div>
+        </div>
+    </div>
+     <script src="assets/js/Outay.js"></script> 
 </body>
 </html>
