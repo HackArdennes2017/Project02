16875a08fd7f4962b4efcbd1729b298398977de0
Carl Christian Lexow <cc.lexow@gmail.com>
2017-08-27 07:21:21 +0200

    Added functionality to let a compass needle point towards the lost person (2nd upload)

--- WebApplication/View/boussolle.php
+++ WebApplication/View/boussolle.php
@@ -3,7 +3,98 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
+  <script>
+
+           var latHelpingPerson;
+           var longHelpingPerson;
+           var latLostPerson;
+           var longLostPerson;
+
+            function getPersonposition(position) {
+
+              latHelpingPerson = position.coords.latitude;
+              longHelpingPerson = position.coords.longitude;
+
+              latLostPerson = 49.762114;
+              longLostPerson = 4.715107;
+
+            }
+
+            //calculates the distance between two coordinates in [km]
+            function CalculateDistance(latLostPerson, longLostPerson, latHelpingPerson, longHelpingPerson){
+              // constants for distance between meridians and circles of latitude
+              var cDLat  = 111.3;
+              var cDLong = 71.5;
+
+              // calculate distance in latitude and longitude direction
+              var dx = cDLong * (longLostPerson - longHelpingPerson);
+              var dy = cDLat * (latLostPerson - latHelpingPerson);
+
+              // calculate the distance between these pg_connection_status
+              var distance = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+              // return calculated distance
+              //dataDistance.innerHTML = distance
+
+              //return distance;
+            }
+
+            // calculates the bearing angle from the searching person towards the lost person in [degrees]
+            function CalculateAngleTowardsLostPerson(latLostPerson, longLostPerson, latHelpingPerson, longHelpingPerson){
+              // calculate the bearing angle using the haversine formula
+              // first convert given coordinates from degrees to radians
+              var phiB    = latHelpingPerson * Math.PI / 180;
+              var phiA    = latLostPerson * Math.PI / 180;
+              var lambdaB = longHelpingPerson * Math.PI / 180;
+              var lambdaA = longLostPerson * Math.PI / 180;
+              // calculate bearing angle using haversine formula
+              var y=Math.sin(lambdaB - lambdaA)*Math.cos(phiB);
+              var x=Math.cos(phiA)*Math.sin(phiB)-Math.sin(phiA)*Math.cos(phiB)*Math.cos(lambdaB - lambdaA);
+              var brng = Math.atan2(y,x)*180/Math.PI;
+              // map angle to range [0 360]
+
+              brng = (brng+180)%360;
+              return brng;
+            }
+
+            var lastDeltaAngle = 0;
+
+            function init() {
+              var deltat;
+              var kangle;
+              var positionBoussole=0;
+
+              CalculateDistance(latLostPerson, longLostPerson, latHelpingPerson, longHelpingPerson);
+
+
+              //Check for support for DeviceOrientation event
+              if(window.DeviceOrientationEvent) {
+                        window.addEventListener('deviceorientation', function(event) {
+                        var alpha = event.alpha;
+                        var beta = event.beta;
+                        var gamma = event.gamma;
+
+                        console.log( "alpha : "+alpha );
+                        console.log( "beta : "+beta );
+                        console.log( "gamma : "+gamma );
+
+
+                        if(alpha!=null || beta!=null || gamma!=null){
+                          kangle = CalculateAngleTowardsLostPerson(latLostPerson, longLostPerson, latHelpingPerson, longHelpingPerson);
+                          deltat = -(kangle-alpha) + 135;
+                          lastDeltaAngle = deltat;
+                        }
+                         // dataContainerOrientation.innerHTML = 'alpha: ' + alpha + '<br/>beta: ' + beta + '<br />gamma: ' + gamma;
+
+
+                         $('img').css({'transform':'rotate('+lastDeltaAngle+'deg)'});
+                      }, false);
+              }
+
+            }
+
+    </script>
 <style>
 
 html {
@@ -60,6 +151,9 @@ input[type=text], select {
   font-style: normal;
   color: #d0cccc;
 }
+.imgboussole{
+
+}
 </style>
 	<title>Test</title>
 </head>
@@ -73,13 +167,26 @@ input[type=text], select {
 
 
 
-<section class="SectionStyle">
+  <section class="SectionStyle">
 
-Boussolle
-</section>
+  <img class="imgboussole" src="../assets/img/backgrounds/fleche_haut.png">
+
+  </section>
 <footer class="FooterStyle">
   Messages / Liste des copains
 </footer>
 
 </body>
 </html>
+
+<script>
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+
+if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(getPersonposition);
+});
+init();
+
+</script>
